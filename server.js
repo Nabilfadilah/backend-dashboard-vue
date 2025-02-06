c205924f3dba80faa0e2a08d5d04347af200939a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-06 10:42:33 +0700

    Menambah app use route loan

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const sequelize = require("./config/database");
 const aboutRoutes = require("./routes/aboutRoutes");
+const peminjamanRoutes = require("./routes/peminjamanRoutes");
 
 // Import model agar Sequelize mengenali tabel
 const peminjaman = require("./models/peminjaman");
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/about", aboutRoutes);
+app.use("/api/loan", peminjamanRoutes);
 
 // sequelize.sync({ force: false }).then(() => {
 //     console.log("Database & tables created!");
