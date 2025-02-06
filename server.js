9d9ddea8939cddd74c8ca22c595206c784301aa2
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-06 09:28:14 +0700

    Menambah kondisi untuk membuat atau memperbaharui table

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const sequelize = require("./config/database");
 const aboutRoutes = require("./routes/aboutRoutes");
 
+// Import model agar Sequelize mengenali tabel
+const peminjaman = require("./models/peminjaman");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,8 +16,19 @@ app.use("/api/about", aboutRoutes);
 //     console.log("Database & tables created!");
 // });
 
+// alter: true → Hanya memperbarui struktur tabel yang berubah (tidak menghapus data).
+// force: true → Akan menghapus tabel lama dan membuat ulang (jangan gunakan jika ada
+
 const PORT = 5001;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
-  await sequelize.sync();
+  // await sequelize.sync();
+
+  // Membuat atau memperbarui tabel sesuai model
+  try {
+    await sequelize.sync({ alter: true });
+    console.log("Database & tables synced!");
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
 });
